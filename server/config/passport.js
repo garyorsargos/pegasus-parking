a904c3cb650d1b23db9b410526dd91426cc53980
pedro criado <pedrocriadof2004@gmail.com>
2024-11-24 21:47:33 -0500

    added Sean's local storage passport design to current code

---
+++ server/config/passport.js
@@ -0,0 +1,35 @@
+const LocalStrategy = require('passport-local').Strategy;
+const Users = require('../models/user');
+
+module.exports = (passport) => {
+    passport.use(new LocalStrategy(
+        async (userName, password, cb) => {
+            try {
+                const user = await Users.findOne({ userName: userName });
+                if (!user) {
+                    return cd(null, false, { message: 'Username not found.' });
+                }
+                // hashes password with sha2-256 encryption using a randomly generated user's salt value to make it more secure from rainbow tables
+                const valid = user.authenticate(password);
+                if (!valid) {
+                    return cb(null, false, { message: "Incorrect Password." });
+                }
+                return cb(null, user);
+            } catch (error) {
+                return cb(error);
+            }
+        }
+    ));
+
+    passport.serializeUser((user, cb) => {
+        process.nextTick(function() {
+            cb(null, { id: user.userId, username: user.userName });
+        })
+    });
+
+    passport.deserializeUser((user, cb) => {
+        process.nextTick(function() {
+            return cb(null, user);
+        });
+    });
+};
--- server/login.js
+++ server/login.js
@@ -1,17 +1,40 @@
 const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
 const connectToDatabase = require('./config/mongodb');
+const passportConfig = require('./config/passport');
+const { login, register, logout } = require("./controllers/auth");
 const app = express();
-const { login, register } = require("./controllers/auth");
 
 app.use(express.json());
+
+app.use(session({
+  secret: 'g3rb3r1$cr4zy*()43', // random key I made, should be changed
+  resave: false,
+  saveUninitialized: true,
+  // uncomment when used on https
+  // cookie: {
+  //   secure: true, 
+  //   httpOnly: true, 
+  //   sameSite: 'strict',
+  //},
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //connect to the mongodb PegasusParking database through mongoose.
 connectToDatabase();
 
+//Passport Configuration
+passportConfig(passport);
+
+
+
 // login/register a user
 app.use('/api/login', login);
 app.use('/api/register', register);
-
+app.use('/api/logout', logout);
 
 const PORT = 3000;
 app.listen(PORT, () => {
