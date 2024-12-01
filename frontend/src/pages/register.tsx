0eb8f8832ef8715bbc8d898b13b3bc5f111b9943
Gary Orsargos <garman9813@gmail.com>
2024-12-01 17:49:09 -0500

    Added register page

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -1,70 +1,109 @@
-import { Component } from "react";
+import { useState } from "react";
 import { Box, Heading, Input, Button, Stack, Text } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
-export default class Register extends Component {
-  render() {
-    return (
-      <Box
-        bg="white"
-        shadow="sm"
-        position="absolute"
-        top="50%"
-        left="50%"
-        transform="translate(-50%, -50%)"
-        width="50vw"
-        height="65vh"
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
-        borderRadius="5px"
-        p="6"
-      >
-        <Heading as="h1" size="5xl" mb="2" color="black">
-          Pegasus Parking
-        </Heading>
-        <Text fontSize="lg" color="gray.500" mb="6">
+const Register = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleRegister = async () => {
+    try {
+      const response = await axios.post(
+        "https://parking.garyorsargos.xyz/api/register",
+        { firstName, lastName, userName: username, password },
+        { withCredentials: true }
+      );
+
+      if (response.status === 200) {
+        navigate("/user/parking");
+      }
+    } catch (error) {
+      console.error("Error registering:", error);
+    }
+  };
+
+  return (
+    <Box
+      bg="white"
+      shadow="sm"
+      position="absolute"
+      top="50%"
+      left="50%"
+      transform="translate(-50%, -50%)"
+      width="50vw"
+      height="65vh"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      borderRadius="5px"
+      p="6"
+    >
+      <Heading as="h1" size="5xl" mb="2" color="black">
+        Pegasus Parking
+      </Heading>
+      <Text fontSize="lg" color="gray.500" mb="6">
+        Register
+      </Text>
+      <Stack width="100%" maxWidth="sm">
+        <Input
+          placeholder="First Name"
+          size="lg"
+          borderColor="gray.300"
+          color="black"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <Input
+          placeholder="Last Name"
+          size="lg"
+          borderColor="gray.300"
+          color="black"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <Input
+          placeholder="Username"
+          size="lg"
+          borderColor="gray.300"
+          color="black"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <Input
+          placeholder="Password"
+          type="password"
+          size="lg"
+          borderColor="gray.300"
+          color="black"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button
+          bg="#3067bf"
+          color="white"
+          size="lg"
+          width="100%"
+          onClick={handleRegister}
+        >
           Register
-        </Text>
-        <Stack width="100%" maxWidth="sm">
-          <Input
-            placeholder="First Name"
-            size="lg"
-            borderColor="gray.300"
-            color="black"
-          />
-          <Input
-            placeholder="Last Name"
-            size="lg"
-            borderColor="gray.300"
-            color="black"
-          />
-          <Input
-            placeholder="Username"
-            size="lg"
-            borderColor="gray.300"
-            color="black"
-          />
-          <Input
-            placeholder="Password"
-            type="password"
-            size="lg"
-            borderColor="gray.300"
-            color="black"
-          />
-          <Button bg="#3067bf" color="white" size="lg" width="100%">
-            Register
-          </Button>
-        </Stack>
-        <Text mt={4}>
-          <Link to="/login" style={{ color: "#3067bf", textDecoration: "underline" }}>
-            Back to login page
-          </Link>
-        </Text>
-      </Box>
-    );
-  }
-}
+        </Button>
+      </Stack>
+      <Text mt={4}>
+        <Link
+          to="/login"
+          style={{ color: "#3067bf", textDecoration: "underline" }}
+        >
+          Back to login page
+        </Link>
+      </Text>
+    </Box>
+  );
+};
 
+export default Register;
 
